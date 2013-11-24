78a43d1c4afccb1b0dd03ab03fd96447240e2a6a
Lorenzo Pisani <zeelot3k@gmail.com>
2013-11-23 23:35:33 -0800

    more code in the stocks package

--- sharpefolio/stocks.py
+++ sharpefolio/stocks.py
@@ -26,6 +26,9 @@ class StockMapper:
 	def find_by_symbol(self, symbol):
 		return self.repository.find_by_symbol(symbol)
 
+	def find_all(self):
+		return self.repository.find_all()
+
 class StockSqliteRepository:
 	def __init__(self, database):
 		self.database = database
@@ -40,6 +43,10 @@ class StockSqliteRepository:
 
 		return self.build_model(result)
 
+	def find_all(self):
+		cursor = self.database.execute('SELECT * FROM `stocks`')
+		return StockCollection(cursor)
+
 	def build_model(self, data):
 		if (data == None):
 			return None
@@ -47,6 +54,23 @@ class StockSqliteRepository:
 		model = Stock(data['symbol'], data['company'])
 		model._id = data['id']
 
+		return model;
+
+class StockCollection:
+	def __init__(self, stocks):
+		self._stocks = stocks
+
+	def __iter__(self):
+		self._stocks.__iter__()
+		return self;
+
+	def next(self):
+		next = self._stocks.next()
+		return self.build_model(next)
+
+	def build_model(self, data):
+		model = Stock(data['symbol'], data['company'])
+		model._id = data['id']
 		return model;
 
 class Price(object):
@@ -102,3 +126,27 @@ class PriceSqliteRepository:
 			(model.stock_id, model.year, model.month, model.day, model.closing_price, model.change)
 		)
 		self.database.commit()
+
+class PriceCollection:
+	def __init__(self, prices):
+		self._prices = prices
+
+	def __iter__(self):
+		self._prices.__iter__()
+		return self;
+
+	def next(self):
+		next = self._prices.next()
+		return self.build_model(next)
+
+	def build_model(self, data):
+		model = Price(
+			data['stock_id'],
+			data['year'],
+			data['month'],
+			data['day'],
+			data['closing_price'],
+			data['change']
+		)
+		model._id = data['id']
+		return model;

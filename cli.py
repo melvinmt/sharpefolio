b8beb4f11e24d4d14edcc89b2ae87019f4b954f5
Lorenzo Pisani <zeelot3k@gmail.com>
2013-11-23 23:35:48 -0800

    update cli script to load historical prices data

--- cli.py
+++ cli.py
@@ -16,37 +16,31 @@ stock_mapper = stocks.StockMapper(stock_repository)
 price_repository = stocks.PriceSqliteRepository(connection)
 price_mapper = stocks.PriceMapper(price_repository)
 
-stock = stock_mapper.find_by_symbol('AAPL')
-#price = stocks.Price(stock.id, 2013, 4, 20, 123.12, 12.1)
-
-#price_mapper.insert(price)
-
 url = 'http://query.yahooapis.com/v1/public/yql?'
 params = {'format': 'json', 'env': 'store://datatables.org/alltableswithkeys', 'q': None}
-
 query = 'select * from yahoo.finance.historicaldata where symbol = "%s" and startDate = "%04d-%02d-1" and endDate = "%04d-%02d-1"'
 
-for year in xrange(2003,2013+1):
-	for month in xrange(1, 12+1):
-		if (month == 12):
-			params['q'] = query % (stock.symbol, year, month, year + 1, 1)
-		else:
-			params['q'] = query % (stock.symbol, year, month, year, month + 1)
+stocks_collection = stock_mapper.find_all()
+for stock in stocks_collection:
+	for year in xrange(2012,2013+1):
+		params['q'] = query % (stock.symbol, year, 1, year + 1, 1)
 		result = urllib2.urlopen(url+urllib.urlencode(params))
 		data = json.load(result)
 
 		if data['query']['results'] == None:
-			print 'skipping empty result'
+			print 'skipping empty result', stock.symbol, 'for', year
 			continue
 
 		for info in data['query']['results']['quote']:
 			close = info['Adj_Close']
 			date = datetime.datetime.strptime(info['date'], '%Y-%m-%d')
 			day = date.day
+			month = date.month
 			try:
-				print stock.symbol, 'for', year,month,day
 				price = stocks.Price(stock.id, year, month, day, close, 0)
 				price_mapper.insert(price)
 			except Exception, e:
-				print 'already inserted', stock.symbol, 'for', year,month,day
+				print 'already inserted', stock.symbol, 'for', year, month, day, '(', e ,')'
 				pass
+			else:
+				print 'successfully imported', stock.symbol, 'for', year, month, day

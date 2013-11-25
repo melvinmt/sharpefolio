d7635460b8301e9aad16218bde38775bb15d8c81
Lorenzo Pisani <zeelot3k@gmail.com>
2013-11-24 18:36:41 -0800

    slightly better error handling

--- cli.py
+++ cli.py
@@ -23,24 +23,31 @@ query = 'select * from yahoo.finance.historicaldata where symbol = "%s" and star
 stocks_collection = stock_mapper.find_all()
 for stock in stocks_collection:
 	for year in xrange(2012,2013+1):
-		params['q'] = query % (stock.symbol, year, 1, year + 1, 1)
-		result = urllib2.urlopen(url+urllib.urlencode(params))
-		data = json.load(result)
+		try:
+			params['q'] = query % (stock.symbol, year, 1, year + 1, 1)
+			result = urllib2.urlopen(url+urllib.urlencode(params))
+			data = json.load(result)
+		except urllib2.HTTPError, e:
+			print 'HTTP Error:', e, url+urllib.urlencode(params)
+			raise e
+		else:
+			if data['query']['results'] == None:
+				print 'skipping empty result', stock.symbol, 'for', year
+				continue
 
-		if data['query']['results'] == None:
-			print 'skipping empty result', stock.symbol, 'for', year
-			continue
-
-		for info in data['query']['results']['quote']:
-			close = info['Adj_Close']
-			date = datetime.datetime.strptime(info['date'], '%Y-%m-%d')
-			day = date.day
-			month = date.month
-			try:
-				price = stocks.Price(stock.id, year, month, day, close, 0)
-				price_mapper.insert(price)
-			except Exception, e:
-				print 'already inserted', stock.symbol, 'for', year, month, day, '(', e ,')'
-				pass
-			else:
-				print 'successfully imported', stock.symbol, 'for', year, month, day
+			for info in data['query']['results']['quote']:
+				try:
+					close = info['Adj_Close']
+					date = datetime.datetime.strptime(info['date'], '%Y-%m-%d')
+					day = date.day
+					month = date.month
+					price = stocks.Price(stock.id, year, month, day, close, 0)
+					price_mapper.insert(price)
+				except Exception, e:
+					print 'error inserting', stock.symbol, 'for', year, '(', e ,')'
+					pass
+				except urllib2.HTTPError, e:
+					print 'woo', e
+					raise e
+				else:
+					print 'successfully imported', stock.symbol, 'for', year, month, day

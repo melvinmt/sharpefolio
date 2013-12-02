12257338b015625c229c19a5d42cdbebafa1fd45
Lorenzo Pisani <zeelot3k@gmail.com>
2013-12-01 21:08:39 -0800

    update schema.sql file

--- schema.sql
+++ schema.sql
@@ -4,4 +4,12 @@ CREATE  TABLE `sharpefolio`.`prices` (
   `date` DATE NOT NULL ,
   `closing_price` DOUBLE(10,4) UNSIGNED NOT NULL ,
   `change` DOUBLE(14,8) NOT NULL DEFAULT 0 ,
-  PRIMARY KEY (`id`, `stock_id`, `date`) );
+  PRIMARY KEY (`id`, `stock_id`, `date`)
+) ENGINE=InnoDB AUTO_INCREMENT=7935 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `stocks` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `symbol` varchar(50) NOT NULL,
+  `company` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`,`symbol`)
+) ENGINE=InnoDB AUTO_INCREMENT=7935 DEFAULT CHARSET=utf8;

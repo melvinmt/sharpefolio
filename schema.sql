f3084496b9983fca79b4ef71ffbd1e9b25294d94
Lorenzo Pisani <zeelot3k@gmail.com>
2013-12-01 21:09:13 -0800

    don't set auto increment value

--- schema.sql
+++ schema.sql
@@ -5,11 +5,11 @@ CREATE  TABLE `sharpefolio`.`prices` (
   `closing_price` DOUBLE(10,4) UNSIGNED NOT NULL ,
   `change` DOUBLE(14,8) NOT NULL DEFAULT 0 ,
   PRIMARY KEY (`id`, `stock_id`, `date`)
-) ENGINE=InnoDB AUTO_INCREMENT=7935 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `stocks` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `symbol` varchar(50) NOT NULL,
   `company` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`id`,`symbol`)
-) ENGINE=InnoDB AUTO_INCREMENT=7935 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
